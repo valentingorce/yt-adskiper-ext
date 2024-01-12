4b8a494594f58e9c37b8ca7818277ef37dc74ea4
valentin gorce <valentin.gorce97@gmail.com>
2024-01-12 20:57:19 +0100

    finally working

--- yt-adskiper-ext/content-script.js
+++ yt-adskiper-ext/content-script.js
@@ -1,12 +1,13 @@
 function findAndClickButton() {
     console.log('There 2');
     var buttonText = "Ignorer";
-    var buttonClass = "ytp-ad-skip-button-modern ytp-button";
+    var buttonClass = "ytp-button";
 
     var buttons = document.querySelectorAll('.' + buttonClass);
 
     buttons.forEach(function (button) {
         if (button.textContent.trim() === buttonText) {
+            console.log('FOUND!!!');
             button.click();
         }
     });
@@ -16,24 +17,5 @@ function findAndClickButton() {
     }
 }
 
-// Wait for the DOM to be ready
-document.addEventListener('DOMContentLoaded', function () {
-    // Find and click the button immediately
-    findAndClickButton();
-    console.log('There 1');
-    // Use MutationObserver to wait for changes in the DOM
-    var observer = new MutationObserver(function (mutations) {
-        console.log('domcontentload event fired');
-        mutations.forEach(function (mutation) {
-            // Check if the button is added to the DOM
-            if (mutation.addedNodes.length > 0) {
-                findAndClickButton();
-            }
-        });
-    });
-
-    // Configure and start the observer
-    observer.observe(document.body, { childList: true, subtree: true });
-    console.log('There 2');
-});
+setInterval(findAndClickButton, 1000); // check the button every second
 console.log('Here I am');
